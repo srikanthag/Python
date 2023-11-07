a4ad598c0b1ef89356c7a127b7b55fe6e9a83484
hp <srikanthag93@gmail.com>
2023-11-07 22:34:45 +0530

    python_notes_13

--- Practice.py
+++ Practice.py
@@ -1,4 +1,5 @@
-numbers = ['857', '987', '8', '120', '888888', '547', '7674', '89', '589', '388888', '2889']
-d = [item for item in numbers if int(item[0]) == 8]
-print(d)
+import os
+
+c = os.path.getsize(r"C:\Users\hp\Desktop\IT\Python\Python\Comprehension")
+print(c)
 
