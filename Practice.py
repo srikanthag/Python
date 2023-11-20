e01cc56f7b37c017ea845d08674fff6ac49ab2c4
hp <srikanthag93@gmail.com>
2023-11-20 23:00:48 +0530

    python_notes_24

--- Practice.py
+++ Practice.py
@@ -1,4 +1,15 @@
+class Student:
+    name = 'unknown' # class attribute
 
+    def __init__(self):
+        self.age = 20  # instance attribute
+
+    @staticmethod
+    def tostring():
+        print('Student Class')
+
+std = Student()
+print(std.tostring())
 
 
 
